b4aaca89eca2b69029e80967653c162d75eecc64
Tanachuns <tanarak.chuns@gmail.com>
2025-02-05 15:01:47 +0700

    change site

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "custom-mint": "#98fbcb",
+      },
+      fontFamily: {
+        sans: ["Noto Sans Thai", "Kanit", "sans-serif"],
+        kanit: ["Kanit", "sans-serif"],
+        noto: ["Noto Sans Thai", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
